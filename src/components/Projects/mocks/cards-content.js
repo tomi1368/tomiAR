952560671677eda74a87efdd95e2d009529665b6
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2024-02-15 03:18:24 -0300

    new work

--- src/components/Projects/mocks/cards-content.js
+++ src/components/Projects/mocks/cards-content.js
@@ -1,70 +1,154 @@
 const cardsContent = [
-    {
-        team:"Individual",
-        title:"MERN-Redux Amazon",
-        subtitle:"An Amazon clone developed in React. Payment System was made with Mercado Pago API",
-        tags:["ReactJS","MongoDB","Mercado Pago API","Mongo Atlas","React Slick","Express","ReactHooks","React-Router","Responsive Design","JWT","Bcrypt"],
-        img:"https://i.ibb.co/LgXSL3D/2022-01-10-14h11-03.png",
-        url:"https://mern-amazon.netlify.app/"
-    },
-    {
-        team:"Individual",
-        title:"Wallet Alkemy",
-        subtitle:"My Wallet app shows you everything you want to see in order to understand where you've been spending your money",
-        tags:["ReactJS","MongoDB","Mongo Atlas","Express","ReactHooks","React-Router","Responsive Design","JWT","Bcrypt","SASS"],
-        img:"https://i.ibb.co/Hds2XVH/2022-03-23-08h29-55.png",
-        url:"https://walletalkemy.netlify.app/"
-    },
-    {
-        team:"Individual",
-        title:"Image Upload on AWS",
-        subtitle:"Using Splash API you can upload an image to AWS S3 bucket",
-        tags:["Express","MongoDB","Mongo Atlas","AWS S3","Unsplash API","JWT","Bcrypt"],
-        img:"https://niixer.com/wp-content/uploads/2020/10/amazon-s3.png",
-        url:"https://github.com/tomi1368/aluxion-server"
-    },
-    {
-        team:"Individual",
-        title:"Referral System",
-        subtitle:"Invite your friends and earn $500",
-        tags:["ReactJS","MongoDB","Mongo Atlas","Express","ReactHooks","React-Router","Responsive Design","JWT","Bcrypt","SASS"],
-        img:"https://www.pichincha.com/portal/portals/0/adam/pichinchablog/52jkxfv-qu6jwdqh3m36rg/image/friends.jpg",
-        url:"https://github.com/tomi1368/currencySpa"
-    },
-    
-    {
-        team:"Individual",
-        title:"QUIZ React-TS",
-        subtitle:"Video Games Quizzes. Think you are a true Video Games expert? Take a quiz below to find out if you're right.",
-        tags:["ReactJS","React Hooks", "TypeScript","SASS"],
-        img:"https://i.ibb.co/vd3CWwy/2022-02-13-19h09-57.png",
-        url:"https://game-quiz-reactts.netlify.app"
-    },
-    {
-        team:"Groupal",
-        title:"Rick & Morty App",
-        subtitle:"App based on Rick and Morty series. This app allow you to Interact and Learn about Rick and Morty's universe. If you are an unregisted user, you'll only can see the content. But, if you are a registed user, you'll also can modified the content",
-        tags:["ReactJS","MongoDB","Mongo Atlas","Express","ReactHooks","React-Router","Responsive Design","JWT","Bcrypt"],
-        img:"https://staticuestudio.blob.core.windows.net/buhomag/2017/01/26010223/rick-y-morty-3.jpg",
-        url:"https://wiki-rickandmorty.netlify.app/"
-    },
-    {   
-        team:"Individual",
-        title:"MERN-CRUD",
-        subtitle:"Create-Read-Update-Delete any user that you want. You can search one too",
-        tags:["ReactJS","MongoDB","Mongo Atlas","Express","ReactHooks","React-Router","Responsive Design","JWT","Bcrypt"],
-        img:"https://i.ibb.co/0ycYLr3/2021-12-22-14h25-47.png",
-        url:"https://tommern-crud.netlify.app"
-    },
-    {
-        team:"Individual",
-        title:"Redux CRUD",
-        subtitle:" It´s a Users CRUD but this one was made with Redux",
-        tags:["Redux","MongoDB","ReactJS","Express","ReactHooks","React-Router","Responsive Design"],
-        img:"https://i.ibb.co/HD03QqY/2021-12-22-16h25-24.png",
-        url:"https://tomi1368.github.io/automarket.github.io/"
-    },
-    /* {
+  {
+    team: "Remote Work",
+    title: "FullStack Developer at DevPlace",
+    subtitle:
+      "Over the past two years, I have been actively involved with a startup focusing on the educational sector, providing learning services and course sales/subscriptions. Our application has successfully garnered registrations from over 10,000 individuals, noteworthy for its concurrent traffic management. Given the nature of being a streaming service, the efficient handling of multimedia file traffic generated through user interaction has proven to be pivotal",
+    tags: ["ReactJS", "NodeJS", "Express", "NestJS", "MongoDB", "React Hooks"],
+    img: "https://scontent.ffdo5-1.fna.fbcdn.net/v/t39.30808-6/216458491_107557924931265_3272774921282004291_n.png?_nc_cat=101&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=aqMnqFraWuAAX9DIeNK&_nc_ht=scontent.ffdo5-1.fna&oh=00_AfCG6PPEeqanTc2dneejxADvCwE4IDLftCqO-ETfcJISfA&oe=65D37E75",
+    url: "https://www.devplace.io/",
+  },
+  {
+    team: "Individual",
+    title: "MERN-Redux Amazon",
+    subtitle:
+      "An Amazon clone developed in React. Payment System was made with Mercado Pago API",
+    tags: [
+      "ReactJS",
+      "MongoDB",
+      "Mercado Pago API",
+      "Mongo Atlas",
+      "React Slick",
+      "Express",
+      "ReactHooks",
+      "React-Router",
+      "Responsive Design",
+      "JWT",
+      "Bcrypt",
+    ],
+    img: "https://i.ibb.co/LgXSL3D/2022-01-10-14h11-03.png",
+    url: "https://mern-amazon.netlify.app/",
+  },
+  {
+    team: "Individual",
+    title: "Wallet Alkemy",
+    subtitle:
+      "My Wallet app shows you everything you want to see in order to understand where you've been spending your money",
+    tags: [
+      "ReactJS",
+      "MongoDB",
+      "Mongo Atlas",
+      "Express",
+      "ReactHooks",
+      "React-Router",
+      "Responsive Design",
+      "JWT",
+      "Bcrypt",
+      "SASS",
+    ],
+    img: "https://i.ibb.co/Hds2XVH/2022-03-23-08h29-55.png",
+    url: "https://walletalkemy.netlify.app/",
+  },
+  {
+    team: "Individual",
+    title: "Image Upload on AWS",
+    subtitle: "Using Splash API you can upload an image to AWS S3 bucket",
+    tags: [
+      "Express",
+      "MongoDB",
+      "Mongo Atlas",
+      "AWS S3",
+      "Unsplash API",
+      "JWT",
+      "Bcrypt",
+    ],
+    img: "https://niixer.com/wp-content/uploads/2020/10/amazon-s3.png",
+    url: "https://github.com/tomi1368/aluxion-server",
+  },
+  {
+    team: "Individual",
+    title: "Referral System",
+    subtitle: "Invite your friends and earn $500",
+    tags: [
+      "ReactJS",
+      "MongoDB",
+      "Mongo Atlas",
+      "Express",
+      "ReactHooks",
+      "React-Router",
+      "Responsive Design",
+      "JWT",
+      "Bcrypt",
+      "SASS",
+    ],
+    img: "https://www.pichincha.com/portal/portals/0/adam/pichinchablog/52jkxfv-qu6jwdqh3m36rg/image/friends.jpg",
+    url: "https://github.com/tomi1368/currencySpa",
+  },
+
+  {
+    team: "Individual",
+    title: "QUIZ React-TS",
+    subtitle:
+      "Video Games Quizzes. Think you are a true Video Games expert? Take a quiz below to find out if you're right.",
+    tags: ["ReactJS", "React Hooks", "TypeScript", "SASS"],
+    img: "https://i.ibb.co/vd3CWwy/2022-02-13-19h09-57.png",
+    url: "https://game-quiz-reactts.netlify.app",
+  },
+  {
+    team: "Groupal",
+    title: "Rick & Morty App",
+    subtitle:
+      "App based on Rick and Morty series. This app allow you to Interact and Learn about Rick and Morty's universe. If you are an unregisted user, you'll only can see the content. But, if you are a registed user, you'll also can modified the content",
+    tags: [
+      "ReactJS",
+      "MongoDB",
+      "Mongo Atlas",
+      "Express",
+      "ReactHooks",
+      "React-Router",
+      "Responsive Design",
+      "JWT",
+      "Bcrypt",
+    ],
+    img: "https://staticuestudio.blob.core.windows.net/buhomag/2017/01/26010223/rick-y-morty-3.jpg",
+    url: "https://wiki-rickandmorty.netlify.app/",
+  },
+  {
+    team: "Individual",
+    title: "MERN-CRUD",
+    subtitle:
+      "Create-Read-Update-Delete any user that you want. You can search one too",
+    tags: [
+      "ReactJS",
+      "MongoDB",
+      "Mongo Atlas",
+      "Express",
+      "ReactHooks",
+      "React-Router",
+      "Responsive Design",
+      "JWT",
+      "Bcrypt",
+    ],
+    img: "https://i.ibb.co/0ycYLr3/2021-12-22-14h25-47.png",
+    url: "https://tommern-crud.netlify.app",
+  },
+  {
+    team: "Individual",
+    title: "Redux CRUD",
+    subtitle: " It´s a Users CRUD but this one was made with Redux",
+    tags: [
+      "Redux",
+      "MongoDB",
+      "ReactJS",
+      "Express",
+      "ReactHooks",
+      "React-Router",
+      "Responsive Design",
+    ],
+    img: "https://i.ibb.co/HD03QqY/2021-12-22-16h25-24.png",
+    url: "https://tomi1368.github.io/automarket.github.io/",
+  },
+  /* {
         team:"Individual",
         title:"Redux Shopping Cart",
         subtitle:"MERN Shopping Cart using Redux to communicate the components",
@@ -72,14 +156,15 @@ const cardsContent = [
         img:"https://imagekit.androidphoria.com/wp-content/uploads/como-buscar-una-imagen-en-facebook-con-una-foto.jpg",
         url:"https://tomi1368.github.io/automarket.github.io/"
     }, */
-    {
-        team:"Individual",
-        title:"Dev Place Challenge",
-        subtitle:"A website about car sales. With this project I enter in DevPlace",
-        tags:["HTML5","SASS","JavaScript","SweetAlertJS","Responsive Design"],
-        img:"https://i.ibb.co/9g457zQ/2021-12-22-12h57-20.png",
-        url:"https://tomi1368.github.io/automarket.github.io/"
-    }
-]
+  {
+    team: "Individual",
+    title: "Dev Place Challenge",
+    subtitle:
+      "A website about car sales. With this project I enter in DevPlace",
+    tags: ["HTML5", "SASS", "JavaScript", "SweetAlertJS", "Responsive Design"],
+    img: "https://i.ibb.co/9g457zQ/2021-12-22-12h57-20.png",
+    url: "https://tomi1368.github.io/automarket.github.io/",
+  },
+];
 
-export default cardsContent
+export default cardsContent;
